be12b5bd5e8f05a8f1b83cf169900b9ac2a54f6c
Dmitry Fisenko <dfisenko@amazon.com>
2022-02-01 16:09:21 -0500

    chore: update alias

--- bootstrap/aliases/net.aliases.sh
+++ bootstrap/aliases/net.aliases.sh
@@ -3,4 +3,4 @@
 # IP addresses
 alias 'ip'="dig +short myip.opendns.com @resolver1.opendns.com"
 alias 'ips'="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
-alias 'lscsp'='lsof -P -iTCP -sTCP:LISTEN'
+alias 'lstcp'='lsof -P -iTCP -sTCP:LISTEN'
