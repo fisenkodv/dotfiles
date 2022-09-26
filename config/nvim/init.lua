f9021ec6083bda361ad98faf7cd963638b51cb42
Dmitry Fisenko <fisenkodv@gmail.com>
2022-09-25 20:11:18 -0400

    chore: merge 492652f65bd8f0b6b61960915491cf6a7797463d

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,9 +3,7 @@ require('highlights')
 require('maps')
 require('plugins')
 
-local has = function(x)
-  return vim.fn.has(x) == 1
-end
+local has = vim.fn.has
 local is_mac = has "macunix"
 local is_win = has "win32"
 
