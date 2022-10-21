85bf9dc25415e67c0c34bfa1b9847319747bc3d5
Dmitry Fisenko <fisenkodv@gmail.com>
2022-10-21 10:12:47 -0400

    gchore(nvim): update null-ls config

--- config/nvim/plugin/null-ls.rc.lua
+++ config/nvim/plugin/null-ls.rc.lua
@@ -15,10 +15,10 @@ end
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.diagnostics.eslint_d.with({
+    null_ls.builtins.diagnostics.eslint.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
-    null_ls.builtins.diagnostics.fish
+    null_ls.builtins.diagnostics.zsh
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
