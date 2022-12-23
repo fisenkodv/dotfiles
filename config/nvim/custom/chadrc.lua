a8da8664b3ce610e523a71498eb10d23ad881809
Dmitry Fisenko <fisenkodv@gmail.com>
2022-12-22 21:09:48 -0500

    chore(iterm): uddate config

--- config/nvim/custom/chadrc.lua
+++ config/nvim/custom/chadrc.lua
@@ -10,7 +10,7 @@ M.ui = {
   changed_themes = themes,
   hl_override = highlights.override,
   hl_add = highlights.add,
-  transparency = true,
+  transparency = false,
 }
 
 M.plugins = require "custom.plugins"
