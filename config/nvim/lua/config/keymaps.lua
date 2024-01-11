a35727d934ff9c3f30c87408ebdbcc1cd60165c4
Dmitry Fisenko <fisenkodv@gmail.com>
2024-01-11 16:36:06 -0500

    merge: a0aad5bcf0fde0e8f1b6eabc14a6dd416f4ae39a

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -5,7 +5,19 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
+-- Do things without affecting the registers
 keymap.set("n", "x", '"_x')
+keymap.set("n", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>P", '"0P')
+keymap.set("v", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>c", '"_c')
+keymap.set("n", "<Leader>C", '"_C')
+keymap.set("v", "<Leader>c", '"_c')
+keymap.set("v", "<Leader>C", '"_C')
+keymap.set("n", "<Leader>d", '"_d')
+keymap.set("n", "<Leader>D", '"_D')
+keymap.set("v", "<Leader>d", '"_d')
+keymap.set("v", "<Leader>D", '"_D')
 
 -- Increment/decrement
 keymap.set("n", "+", "<C-a>")
@@ -50,9 +62,13 @@ keymap.set("n", "<C-w><down>", "<C-w>-")
 
 -- Diagnostics
 keymap.set("n", "<C-j>", function()
-  vim.diagnostic.goto_next()
+	vim.diagnostic.goto_next()
 end, opts)
 
 keymap.set("n", "<leader>r", function()
-  require("utils.utils").replaceHexWithHSL()
+	require("utils.utils").replaceHexWithHSL()
+end)
+
+keymap.set("n", "<leader>i", function()
+	require("utils.lsp").toggleInlayHints()
 end)
