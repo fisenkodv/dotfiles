79871d012d6a49516acf028f4ba78897525fa985
Dmitry Fisenko <fisenkodv@gmail.com>
2024-03-11 08:52:43 -0400

    Update nvim-lspconfig.lua

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -16,6 +16,12 @@ return {
 		opts = {
 			---@type lspconfig.options
 			servers = {
+				eslint = {
+					settings = {
+						-- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+						workingDirectories = { mode = "auto" },
+					},
+				},
 				cssls = {},
 				tsserver = {
 					root_dir = function(...)
