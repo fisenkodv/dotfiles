0c379a22e21a60f7342b40ecec8512fde221d1e4
Dmitry Fisenko <fisenkodv@gmail.com>
2024-07-27 19:13:00 -0400

    chore: add yazi config

---
+++ config/zsh/init/006_yazi.init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if type yazi >/dev/null 2>&1; then
+  function yy() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+      builtin cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
+  }
+fi
--- scripts/install.sh
+++ scripts/install.sh
@@ -7,14 +7,14 @@ SOURCE_MODULES_DIR="$PWD/modules"
 TARGET_CONFIG_DIR="$HOME/.config"
 
 link_config() {
-	local source=$1
-	local target=$2
+  local source=$1
+  local target=$2
 
-	echo "Checking '${source}'..."
-	if [ ! -L ${target} ]; then
-		echo "=> Linking '${source}' to '${target}'..."
-		ln -s ${source} ${target}
-	fi
+  echo "Checking '${source}'..."
+  if [ ! -L ${target} ]; then
+    echo "=> Linking '${source}' to '${target}'..."
+    ln -s ${source} ${target}
+  fi
 }
 
 # shell prompt for zsh, fish
@@ -38,3 +38,4 @@ link_config $SOURCE_CONFIG_DIR/iterm2 $TARGET_CONFIG_DIR/iterm2
 link_config $SOURCE_CONFIG_DIR/wezterm $TARGET_CONFIG_DIR/wezterm
 link_config $SOURCE_CONFIG_DIR/zed $TARGET_CONFIG_DIR/zed
 link_config $SOURCE_CONFIG_DIR/helix $TARGET_CONFIG_DIR/helix
+link_config $SOURCE_CONFIG_DIR/yazi $TARGET_CONFIG_DIR/yazi
