9b261b2d15c24abfe909465ac44c0a3fd41acfb5
Dmitry Fisenko <fisenkodv@gmail.com>
2024-09-21 17:00:29 -0400

    chore(alias): remove quote alias

--- config/zsh/aliases/services.aliases.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# get a random quote from https://github.com/fisenkodv/dictum
-alias quote="curl https://api.fisenko.net/v1/quotes/en/random 2>/dev/null | jq -r '(.text + \" (\" + .author.name) + \")\"'"
