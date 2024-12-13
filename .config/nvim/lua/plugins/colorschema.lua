4cfc1287877e23fb488fc702e3eae0dd5e118a8e
Dmitry Fisenko <fisenkodv@gmail.com>
2024-12-13 00:12:28 -0500

    chore: switch back to lazyvim (#17)

---
+++ .config/nvim/lua/plugins/colorschema.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = true,
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-frappe",
+      -- colorscheme = "solarized-osaka",
+      -- colorscheme = "gruvbox",
+    },
+  },
+}
