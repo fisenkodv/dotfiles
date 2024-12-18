d3d4bd3d36a6a6673ab9be8dc3c4726c308a9182
Dmitry Fisenko <fisenkodv@gmail.com>
2024-12-17 20:38:04 -0500

    feat(nvim): configure vim-visual-multi

--- .config/nvim/lua/plugins/vim-visual-multi.lua
+++ .config/nvim/lua/plugins/vim-visual-multi.lua
@@ -1,5 +1,11 @@
 return {
   {
     "mg979/vim-visual-multi",
+    config = function()
+      vim.g.VM_maps = {
+        ["Select Cursor Up"] = "<C-k>",
+        ["Select Cursor Down"] = "<C-j>",
+      }
+    end,
   },
 }
