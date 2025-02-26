10bc96aefda9e40adc73628cb03071d672a98bf3
Dmitry Fisenko <fisenkodv@gmail.com>
2025-02-26 15:42:56 -0500

    chore: asdf->mise

--- .config/zsh/init/002_asdf.init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if type asdf >/dev/null 2>&1; then
-  export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
-  fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
-fi
---
+++ .config/zsh/init/002_mise.init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if type mise >/dev/null 2>&1; then
+  eval "$(mise activate zsh)"
+fi
