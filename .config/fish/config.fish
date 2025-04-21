2cc150129f738bcb24babd694246e8393f3c5de8
Dmitry Fisenko <fisenkodv@gmail.com>
2025-04-20 23:33:34 -0400

    Simplify fish config file structure and remove Starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,8 @@ set fish_greeting ""
 
 set -gx TERM xterm-256color
 
-for file in ~/.config/fish/{init,functions,exports,aliases}/*.fish
-    if test -r $file
-        source "$file"
-    end
+for file in ~/.config/fish/{init,aliases}/*.fish
+  if test -r $file
+    source "$file"
+  end
 end
-
-
-# starship init fish | source
