7f0352843578b02228f3352c0977dcf3d4137f16
Dmitry Fisenko <fisenkodv@gmail.com>
2025-05-28 09:19:15 -0400

    chore(vim): update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -16,11 +16,5 @@ keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
 
--- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
-
 -- Double jj to exit from insert mode
 keymap.set("i", "jj", "<Esc>", opts)
