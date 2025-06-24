1e1a2bf3401be4a8b0d2637aae6b91710181243a
Dmitry Fisenko <fisenkodv@gmail.com>
2025-06-24 09:16:33 -0400

    feat(fzf): update configuration

--- .config/fish/init/002_fzf.init.fish
+++ .config/fish/init/002_fzf.init.fish
@@ -1,12 +1,25 @@
 if type -q fzf
-  fzf --fish | source
+    fzf --fish | source
 
-  # --- setup fzf theme ---
-  set -x FZF_DEFAULT_OPTS " \
-    --color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
-    --color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+    # --- setup fzf theme ---
+    set -x FZF_DEFAULT_OPTS "
+    --color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284
+    --color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf
     --color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
 
-  set -x FZF_CTRL_T_OPTS "--style full --preview 'if test -d {}; eza --tree --color=always {} | head -n 200; else; bat -n --color=always --line-range :500 {}; end'"
-  set -x FZF_ALT_C_OPTS "--style full --preview 'eza --tree --color=always {} | head -200'"
+    set -x FZF_CTRL_R_OPTS "
+    --style full
+    --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+    --color header:italic
+    --header 'Press CTRL-Y to copy command into clipboard'"
+
+    set -x FZF_CTRL_T_OPTS "
+    --style full
+    --preview 'if test -d {}; eza
+    --tree
+    --color=always {} | head -n 200; else; bat -n --color=always --line-range :500 {}; end'"
+
+    set -x FZF_ALT_C_OPTS "
+    --style full
+    --preview 'eza --tree --color=always {} | head -200'"
 end
--- .config/zsh/init/002_fzf.init.sh
+++ .config/zsh/init/002_fzf.init.sh
@@ -4,11 +4,24 @@ if type fzf >/dev/null 2>&1; then
   source <(fzf --zsh)
 
   # --- setup fzf theme ---
-  export FZF_DEFAULT_OPTS=" \
-    --color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
-    --color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
+  export FZF_DEFAULT_OPTS="
+    --color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284
+    --color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf
     --color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
 
-  export FZF_CTRL_T_OPTS="--style full --preview 'if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi'"
-  export FZF_ALT_C_OPTS="--style full --preview 'eza --tree --color=always {} | head -200'"
+  export FZF_CTRL_R_OPTS="
+    --style full
+    --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+    --color header:italic
+    --header 'Press CTRL-Y to copy command into clipboard'"
+
+  export FZF_CTRL_T_OPTS="
+    --style full
+    --preview 'if test -d {}; eza
+    --tree
+    --color=always {} | head -n 200; else; bat -n --color=always --line-range :500 {}; end'"
+
+  export FZF_ALT_C_OPTS="
+    --style full
+    --preview 'eza --tree --color=always {} | head -200'"
 fi
