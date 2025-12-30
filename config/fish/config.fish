1a7ed6bda88b41abd1a65f14f66fc25af77aa983
Dmitry Fisenko <fisenkodv@gmail.com>
2025-12-30 12:13:33 -0500

    chore: load functions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,7 @@ set fish_greeting ""
 
 set -gx TERM xterm-256color
 
-for file in ~/.config/fish/{path,init,aliases}/*.fish
+for file in ~/.config/fish/{path,init,aliases,functions}/*.fish
   if test -r $file
     source "$file"
   end
